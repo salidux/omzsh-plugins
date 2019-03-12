7d9933665104f75c2611dc6237b8aafcebc8203b
Diego Salido <dsalido@gmail.com>
2019-03-12 10:15:15 -0300

    Add `heroku config` alias

--- plugins/heroku-rails/heroku-rails.plugin.zsh
+++ plugins/heroku-rails/heroku-rails.plugin.zsh
@@ -2,6 +2,7 @@ alias hr='heroku run'
 alias hrc='heroku run rails console'
 alias hrr='heroku run rails runner'
 alias hsql='heroku pg:psql'
+alias hconf='heroku config'
 
 
 # Shows the default target application if no `-a` or `--app` flag is used
